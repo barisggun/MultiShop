c38b7b34507eda5a120bf547cb4784b4737fc3b0
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-05-06 18:27:00 +0300

    add discount api project and sql connection

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+
+        //tabloyu veritabanına yansıtmak için bağlantıyı buradan yapacağız
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-TKFJC4RO;initial Catalog=MultiShopDiscountDb;integrated Security=true");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
