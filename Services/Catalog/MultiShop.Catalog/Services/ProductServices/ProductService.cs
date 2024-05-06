6057045ab4289155e2fe079aa26c3df641f0cacd
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-05-06 15:17:37 +0300

    add entity services

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMongoCollection<Product> _ProductCollection;
+        private readonly IMapper _mapper;
+
+        //database setting nesne örneği arkada oluşturulacak.
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+
+            _ProductCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            var value = _mapper.Map<Product>(createProductDto);
+            await _ProductCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteProductAsync(string id)
+        {
+            await _ProductCollection.DeleteOneAsync(x => x.ProductId == id);
+        }
+
+        public async Task<List<ResultProductDto>> GetAllCategoriesAsync()
+        {
+            var values = await _ProductCollection.Find(x => true).ToListAsync();
+            return (_mapper.Map<List<ResultProductDto>>(values));
+        }
+
+        public async Task<GetByIdProductDto> GetByIdProductAsync(string id)
+        {
+            var values = await _ProductCollection.Find<Product>(x => x.ProductId == id).FirstOrDefaultAsync();
+            return (_mapper.Map<GetByIdProductDto>(values));
+        }
+
+        public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            var values = _mapper.Map<Product>(updateProductDto);
+            await _ProductCollection.FindOneAndReplaceAsync(x => x.ProductId == updateProductDto.ProductId, values);
+        }
+
+
+    }
+}
