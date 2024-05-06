cf0258e0ddb0ed62324844d6c10ffcf98deac482
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-05-06 12:57:16 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] //Benzersiz olduğunu bildirmek için
+        public string Id { get; set; }
+
+    }
+}
