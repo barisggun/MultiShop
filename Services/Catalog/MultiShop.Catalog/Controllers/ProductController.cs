2dfa6fc5d935d2d25ae80fda40f28d3fd85f41c2
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-05-06 15:40:20 +0300

    add catalog controllers

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _ProductService;
+        string className = "Product";
+        public ProductController(IProductService ProductService)
+        {
+            _ProductService = ProductService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _ProductService.GetAllCategoriesAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var values = _ProductService.GetByIdProductAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _ProductService.CreateProductAsync(createProductDto);
+            return Ok($" {className} başarıyla eklenedi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _ProductService.DeleteProductAsync(id);
+            return Ok($" {className} başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _ProductService.UpdateProductAsync(updateProductDto);
+            return Ok($" {className} başarıyla güncellendi");
+        }
+    }
+}
