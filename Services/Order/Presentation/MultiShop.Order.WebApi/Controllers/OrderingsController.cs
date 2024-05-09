3e52b6ad5aa4cccd7eba6f7bd127f8db3ca9d586
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-05-09 18:36:15 +0300

    add mediatr ordering cqrs

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+    }
+}
