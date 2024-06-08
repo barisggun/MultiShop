d41cc3fc1a70504407abff4a0bb2d59abdc72520
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-06-08 16:39:32 +0300

    add register controller

--- IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
 
 namespace MultiShop.IdentityServer.Controllers
 {
@@ -7,5 +11,32 @@ namespace MultiShop.IdentityServer.Controllers
     [ApiController]
     public class RegistersController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser
+            {
+               UserName = userRegisterDto.Username,
+               Name = userRegisterDto.Name,
+               Email = userRegisterDto.Email,
+               Surname = userRegisterDto.Surname
+            };
+
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı oluşturuldu");
+            }
+
+            return BadRequest(result.Errors);
+        }
     }
 }
